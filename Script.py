7ce2f193cfc75bcf248a95d8a6a92d2278b2af49
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-04-08 15:11:44 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -141,7 +141,7 @@ Elsa
 
 
 ╔════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗
-▫️<a href=https://t.me/cynitebackup> ᴄʜᴀɴɴᴇʟ </a>
+▫️<a href=https://t.me/iPapkornbetabots> ᴄʜᴀɴɴᴇʟ </a>
 ╚════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝</b>"""
 
     RESTART_TXT = """
