cfee3cef0c953d4ac92162923c34f28b58f6e678
telebot712 <134066099+telebot712@users.noreply.github.com>
2023-05-20 08:59:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/subhan712/Shortner.git /Shortner
+  git clone https://github.com/telebot712/Shortner.git /Shortner
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Shortner
